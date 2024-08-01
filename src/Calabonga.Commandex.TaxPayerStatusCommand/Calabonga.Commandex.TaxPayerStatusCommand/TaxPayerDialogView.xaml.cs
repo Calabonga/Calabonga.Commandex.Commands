df0d34838157f63e4698bad84db80239b8eed135
Sergei Calabonga <calabonga@outlook.com>
2024-08-01 11:45:57 +0700

    First commit

---
+++ src/Calabonga.Commandex.TaxPayerStatusCommand/Calabonga.Commandex.TaxPayerStatusCommand/TaxPayerDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Calabonga.Commandex.Engine;
+
+namespace Calabonga.Commandex.TaxPayerStatusCommand;
+
+/// <summary>
+/// Interaction logic for TaxPayerDialogView.xaml
+/// </summary>
+public partial class TaxPayerDialogView : UserControl, IDialogView
+{
+    public TaxPayerDialogView()
+    {
+        InitializeComponent();
+    }
+}
