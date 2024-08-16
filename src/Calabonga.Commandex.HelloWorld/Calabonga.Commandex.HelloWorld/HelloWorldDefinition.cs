ce2470a03845204c41792c3a1610d62695359369
Sergei Calabonga <dev@calabonga.net>
2024-08-16 18:23:17 +0700

    Simplest command type for Commandex implemented

---
+++ src/Calabonga.Commandex.HelloWorld/Calabonga.Commandex.HelloWorld/HelloWorldDefinition.cs
@@ -0,0 +1,14 @@
+﻿using Calabonga.Commandex.Engine.Commands;
+using Calabonga.Commandex.HelloWorld.Core;
+using Calabonga.Wpf.AppDefinitions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Calabonga.Commandex.HelloWorld;
+
+public class HelloWorldDefinition : AppDefinition
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<ICommandexCommand, HelloWorldCommandexCommand>();
+    }
+}
