c9b3f1c2577bd79a3cb15f8bcefc2a8317a660d2
Sergei Calabonga <dev@calabonga.net>
2024-09-07 14:48:09 +0700

    Dialog Demo command created

---
+++ src/Calabonga.Commandex.DialogCommand/Calabonga.Commandex.DialogCommand/DialogCommandDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Calabonga.Commandex.DialogCommand.Core.ViewModels;
+using Calabonga.Commandex.DialogCommand.Core.Views;
+using Calabonga.Commandex.Engine.Commands;
+using Calabonga.Wpf.AppDefinitions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Calabonga.Commandex.DialogCommand;
+
+public class DialogCommandDefinition : AppDefinition
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<ICommandexCommand, Core.DialogCommand>();
+        services.AddScoped<DemoDialogResult>();
+        services.AddScoped<DemoDialogView>();
+    }
+}
