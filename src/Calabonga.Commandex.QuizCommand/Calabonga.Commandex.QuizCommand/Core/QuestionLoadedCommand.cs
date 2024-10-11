c9082e669f5d3ab11386a445f6a7f9241d3e2209
Sergei Calabonga <dev@calabonga.net>
2024-10-11 13:39:22 +0700

    Video 12 created

--- src/Calabonga.Commandex.QuizCommand/Calabonga.Commandex.QuizCommand/Core/QuestionLoadedCommand.cs
+++ src/Calabonga.Commandex.QuizCommand/Calabonga.Commandex.QuizCommand/Core/QuestionLoadedCommand.cs
@@ -1,13 +1,16 @@
 ﻿using Calabonga.Commandex.Engine.Commands;
 using Calabonga.Commandex.Engine.Dialogs;
+using Calabonga.Commandex.Engine.Processors.Results;
+using Calabonga.Commandex.QuizCommand.Core.ViewModels;
 using Calabonga.Commandex.QuizCommand.Core.Views;
+using System.Text;
 
 namespace Calabonga.Commandex.QuizCommand.Core;
 
 /// <summary>
 /// // Calabonga: Summary required (QuestionLoadedCommand 2024-08-16 07:20)
 /// </summary>
-public class QuestionLoadedCommand : DialogCommandexCommand<QuizDialogView, ViewModels.QuizViewModel>
+public class QuestionLoadedCommand : DialogCommandexCommand<QuizDialogView, QuizViewModel>
 {
     public QuestionLoadedCommand(IDialogService dialogService) : base(dialogService) { }
 
@@ -21,4 +24,20 @@ public class QuestionLoadedCommand : DialogCommandexCommand<QuizDialogView, View
     /// semver.org principle used
     /// </summary>
     public override string Version => "1.2.0";
+
+    public override bool IsPushToShellEnabled => true;
+
+    public override object GetResult()
+    {
+        var result = (QuizViewModel)Result!;
+        var stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine($"Вопрос: {result.Question!.QuestionText}");
+        stringBuilder.AppendLine($"Из категории: {result.Question!.CategoryName}");
+        stringBuilder.AppendLine($"A: {result.Question!.AnswerA}");
+        stringBuilder.AppendLine($"B: {result.Question!.AnswerB}");
+        stringBuilder.AppendLine($"C: {result.Question!.AnswerC}");
+        stringBuilder.AppendLine($"D: {result.Question!.AnswerD}");
+        stringBuilder.AppendLine($"правильный вариант: {result.Question!.CorrectAnswer.ToString()}");
+        return new ClipboardResult(stringBuilder.ToString());
+    }
 }
