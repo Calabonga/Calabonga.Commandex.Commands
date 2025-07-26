27633fe1e5a62c4291237aea84746421310614cb
Sergei Calabonga <dev@calabonga.net>
2025-07-26 09:34:36 +0700

    ZoneCommandexCommand sample added

---
+++ src/Calabonga.Commandex.ZoneCommand/Calabonga.ZoneCommandexCommand/Core/ZoneCommandCommandexCommand.cs
@@ -0,0 +1,22 @@
+﻿using Calabonga.Commandex.Engine.Commands;
+using Calabonga.Commandex.Engine.Zones;
+using Calabonga.ZoneCommandexCommand.Core.ViewModels;
+using Calabonga.ZoneCommandexCommand.Core.Views;
+
+namespace Calabonga.ZoneCommandexCommand.Core;
+
+public class ZoneCommandCommandexCommand : ZoneCommandexCommand<ZoneCommandView, ZoneCommandViewModel>
+{
+    public ZoneCommandCommandexCommand(IZoneManager zoneManager) : base(zoneManager)
+    {
+    }
+
+    public override string CopyrightInfo => "Calabonga SOFT © 2025";
+
+    public override string DisplayName => "MySecondCommand";
+
+    public override string Description => "My second commandex command for demo";
+
+    public override string Version => GetType().Assembly.GetName().Version?.ToString() ?? "0.0.0";
+}
+
